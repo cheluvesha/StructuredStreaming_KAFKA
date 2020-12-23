f680802687442c79b0b3fb001dc94d28704270a0
cheluvesha <cheluvesh.b@gmail.com>
2020-12-23 14:40:09 +0530

    [CHELUVESHA]Added: CreateSparkSessionObj

---
+++ src/main/scala/com/Utility/UtilityClass.scala
@@ -0,0 +1,29 @@
+package com.Utility
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/***
+  *  Utility class provides useful methods
+  */
+object UtilityClass {
+
+  /** *
+    * creates SparkSession Object
+    * @param appName String
+    * @return SparkSession
+    */
+  def createSparkSessionObj(appName: String): SparkSession = {
+    val sparkConfigurations = new SparkConf()
+      .setAppName(appName)
+      .setMaster("local[*]")
+      .set("spark.streaming.kafka.maxRatePerPartition", "1")
+      .set("spark.streaming.stopGracefullyOnShutdown", "true")
+
+    val sparkSession = SparkSession
+      .builder()
+      .config(sparkConfigurations)
+      .getOrCreate()
+    sparkSession
+  }
+}
